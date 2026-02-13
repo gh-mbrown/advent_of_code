4a2aeef819bb9c62c029ee4a48119d08439067e4
Malcolm Brown <malcolm.brown@brcon.net>
2026-02-12 22:30:23 -0500

    updates

--- fsharp-2024/2024/days/day01.fs
+++ fsharp-2024/2024/days/day01.fs
@@ -4,7 +4,7 @@ open Utils
 
 let private parseInput () =
     lazy
-        (readlines "inputs/day01.txt"
+        (readLines "inputs/day01.txt"
          |> List.map (fun x ->
              x.Split " "
              |> Array.filter (fun y -> y <> "")
